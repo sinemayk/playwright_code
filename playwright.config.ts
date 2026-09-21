eca425ece48c9b34dbd60d5e1a58a136cc929f7f
Sinem <sinemayk@gmail.com>
2026-09-21 11:48:18 +0200

    removed some tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,8 +55,8 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    { name: "iPhone 13", use: { ...devices["iPhone 13"] } },
-    { name: "ipad mini", use: { ...devices["iPad Mini"] } },
+    //{ name: "iPhone 13", use: { ...devices["iPhone 13"] } },
+    //{ name: "ipad mini", use: { ...devices["iPad Mini"] } },
     { name: "setup", testMatch: "**/auth.setup.ts" },
     {
       name: "smoke",
@@ -69,7 +69,7 @@ export default defineConfig({
       testMatch: "**/smoke/*.spec.ts",
       dependencies: ["setup"], //smoke testi calismadan setup calisir
     },
-    {
+   /* {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
@@ -82,7 +82,7 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
